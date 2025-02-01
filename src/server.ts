f536ebdfa4e86dece0d6a28ca05c154d008bdba1
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-01 23:12:50 +0100

    add whitespace for improved readability in Socket.IO connection handling

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,7 @@ db.once("open", () => {
 // Socket.IO Connection
 io.on("connection", (socket) => {
   console.log("Client connected");
+  
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
