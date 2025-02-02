b6b8570543baabe67df0091f6cb6ff7cb208149d
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-02 14:04:00 +0100

    refactor User model to include email and password fields, and remove bookmarks field

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,11 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-export interface IUser extends Document {
-  _id: string;
+interface IUser extends Document {
   username: string;
-  bookmarks: string[];
+  email: string;
+  password: string;
 }
+
 
 const UserSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
