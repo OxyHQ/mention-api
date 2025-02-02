345985801da503bc6ac974c499425a6c3a8af05b
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-02 14:03:43 +0100

    add security middlewares and enhance API response with user and post counts

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,18 @@ import cors from "cors";
 import postsRouter from "./routes/posts";
 import profilesRouter from "./routes/profiles";
 import usersRouter from "./routes/users";
+import authRouter from "./routes/auth";
 import dotenv from "dotenv";
 import fileRoutes from "./routes/files";
 import listsRoutes from "./routes/lists";
 import hashtagsRoutes from "./routes/hashtags";
 import chat from "./routes/chat";
+import User from "./models/User";
+import Post from "./models/Post";
+
+
+// Import security middlewares
+import { rateLimiter, bruteForceProtection, csrfProtection, parseCookies, csrfErrorHandler } from "./middleware/security";
 
 dotenv.config();
 
@@ -23,6 +30,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// Add security middlewares
+app.use(parseCookies);
+app.use(rateLimiter);
+app.use(bruteForceProtection);
+//app.use(csrfProtection);
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI || "");
 
@@ -34,17 +47,27 @@ db.once("open", () => {
 
 // Socket.IO Connection
 io.on("connection", (socket) => {
-  console.log("Client connected");
+  console.log("Client connected from ip: " + socket.handshake.address);
   
-
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
 // API Routes
-app.get("/api", (req, res) => {
-  res.json({ message: "Welcome to the API" });
+app.get("/api", async (req, res) => {
+  try {
+    const usersCount = await User.countDocuments();
+    const postsCount = await Post.countDocuments();
+
+    res.json({
+      message: "Welcome to the API",
+      users: usersCount,
+      posts: postsCount,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching stats", error });
+  }
 });
 
 app.use("/api/posts", postsRouter);
@@ -54,6 +77,10 @@ app.use("/api/files", fileRoutes);
 app.use("/api/lists", listsRoutes);
 app.use("/api/hashtags", hashtagsRoutes);
 app.use("/api/chat", chat(io));
+app.use("/api/auth", authRouter);
+
+// Add CSRF error handler (should be after routes)
+app.use(csrfErrorHandler);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
