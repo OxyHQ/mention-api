a73df8c922b05e913d210890ea1f7fc97429661b
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-08 04:45:42 +0100

    enhance rate limiting and brute force protection middleware to exclude file uploads

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -4,18 +4,20 @@ import csrf from "csurf";
 import cookieParser from "cookie-parser"; // Import cookie-parser
 import { Request, Response, NextFunction } from "express";
 
-// Rate limiting middleware
+// Rate limiting middleware (exclude file uploads)
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 50000, // limit each IP to 100 requests per window
   message: "Too many requests from this IP, please try again later.",
+  skip: (req: Request) => req.path.startsWith('/api/files/upload')
 });
 
-// Brute force protection middleware
+// Brute force protection middleware (exclude file uploads)
 const bruteForceProtection = slowDown({
   windowMs: 15 * 60 * 1000, // 15 minutes
   delayAfter: 50000, // allow 100 requests per 15 minutes, then...
   delayMs: () => 500, // add 500ms delay per request above 100 (new behavior)
+  skip: (req: Request) => req.path.startsWith('/api/files/upload')
 });
 
 // Middleware to parse cookies
