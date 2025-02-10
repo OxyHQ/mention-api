9f7edda64258399bb69937f83f2f26d5a42dc73b
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-10 15:12:09 +0100

    add 'welcome' type to Notification model: extend enum for notification types to include welcome messages

--- src/models/Notification.ts
+++ src/models/Notification.ts
@@ -16,7 +16,7 @@ const NotificationSchema = new Schema({
   type: { 
     type: String, 
     required: true,
-    enum: ['like', 'reply', 'mention', 'follow', 'repost', 'quote']
+    enum: ['like', 'reply', 'mention', 'follow', 'repost', 'quote', 'welcome']
   },
   entityId: { type: Schema.Types.ObjectId, required: true },
   entityType: { 
